8467c50746cae9c55069c312b3cabe36e11790b3
ronald-it <ronald.cirgofer@hotmail.com>
2022-11-05 15:15:03 +0100

    HTML/CSS af behalve footer positionering

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,10 +1,41 @@
 import * as React from 'react';
 import './Login.css';
+import {Link} from "react-router-dom";
 
 export function Login() {
     return (
-        <div>
-            <p>Login</p>
-        </div>
+        <>
+            <main className="main-login">
+                <h3>Login</h3>
+
+                <p>You can log into your account down below</p>
+
+                    <form id="login-form">
+
+                        <label
+                            htmlFor="email"
+                        >
+                            Email address
+                        </label>
+                        <input className="email-input" type="text" id="email" name="email-field"
+                        />
+
+                        <label
+                            htmlFor="password"
+                        >
+                            Password
+                        </label>
+                        <input className="password-input" type="text" id="password" name="password-field"
+                        />
+
+                        <button>
+                            Login
+                        </button>
+
+                    </form>
+
+                <p>Do you not have an account yet? Register <Link to="/registration">here</Link>.</p>
+            </main>
+        </>
     );
 }
--- src/pages/Registration/Registration.js
+++ src/pages/Registration/Registration.js
@@ -1,10 +1,51 @@
 import * as React from 'react';
 import './Registration.css';
+import {Link} from "react-router-dom";
 
 export function Registration() {
     return (
-        <div>
-            <p>Registration</p>
-        </div>
+        <>
+            <main className="main-registration">
+                <h3>Registration</h3>
+
+                <p>You can register an account down below.</p>
+
+                <form id="registration-form">
+
+                    <label
+                        htmlFor="email"
+                    >
+                        Email address
+                    </label>
+                    <input className="email-input" type="text" id="email" name="email-field"
+                    />
+
+                    <label
+                        htmlFor="username"
+                    >
+                        Username
+                    </label>
+                    <input className="username-input" type="text" id="username" name="username-field"
+                    />
+
+                    <label
+                        htmlFor="password"
+                    >
+                        Password
+                    </label>
+                    <input className="password-input" type="text" id="password" name="password-field"
+                    />
+
+                    <button>
+                        Register
+                    </button>
+
+                </form>
+
+                <p>
+                    Do you already have an account? You can login <Link to="/login">here</Link>.
+                </p>
+            </main>
+        </>
     );
 }
